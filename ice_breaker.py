6c3bfe75b402bb025332caedc71feec439cde810
Kaiohenriqueps <kh.silva@hotmail.com>
2025-01-06 16:41:19 -0300

    new ice breaker

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,6 @@
 # python imports
-import os
 from dotenv import load_dotenv
+from agents.linkedin_lookup_agent import lookup
 from third_parties.linkedin import scrape_linkedin_profile
 
 # langchain imports
@@ -9,9 +9,9 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
 
-if __name__ == "__main__":
-    load_dotenv()
-    print("Hello, Langchain!")
+def ice_break_with(name: str) -> str:
+    linkedin_username = lookup(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
 
     summary_template = """
     given the LinkedIn information {information} about a person from I want you to create:
@@ -24,6 +24,11 @@ if __name__ == "__main__":
     )
     llm = ChatOllama(model="llama3.2")
     chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url="", mock=True)
     res = chain.invoke(input={"information": linkedin_data})
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("ICE BREAKER!")
+    ice_break_with(name="Kaio Henrique Pedroza Silva")
