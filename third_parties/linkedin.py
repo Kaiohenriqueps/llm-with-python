ba9cf6bdfdb4be66e2c47800a2eb8146056a9e06
Kaiohenriqueps <kh.silva@hotmail.com>
2025-01-06 15:38:14 -0300

    getting data from linkedin or getting a default mock response (my linkedin profile response)

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -7,7 +7,39 @@ load_dotenv()
 
 def scrape_linkedin_profile(linkedin_profile_url: str, mock: bool = False):
     """
-    Scrape information from linkedin profiles,
+    scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile
     """
-    pass
+    if mock:
+        linkedin_profile_url = "https://gist.githubusercontent.com/Kaiohenriqueps/61ba15f83811669904ce6655ed2b7b31/raw/424b89cd0b83f49827573e606d11391574c01d26/kaio-pedroza.json"
+        response = requests.get(linkedin_profile_url, timeout=10)
+    else:
+        api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
+        header_dic = {"Authorization": f"Bearer {os.environ['PROXYCURL_API_KEY']}"}
+        response = requests.get(
+            api_endpoint,
+            headers=header_dic,
+            params={"url": linkedin_profile_url},
+            timeout=10,
+        )
+
+    data = response.json()
+    data = {
+        k: v
+        for k, v in data.items()
+        if v not in ([], "", "", None)
+        and k not in ["people_also_viewed", "certifications"]
+    }
+    if data.get("groups"):
+        for group_dict in data.get("groups"):
+            group_dict.pop("profile_pic_url")
+
+    return data
+
+
+if __name__ == "__main__":
+    print(
+        scrape_linkedin_profile(
+            linkedin_profile_url="https://www.linkedin.com/in/kaiohenriqueps", mock=True
+        )
+    )
